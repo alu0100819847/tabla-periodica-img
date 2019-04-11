8164cc4de339f28803a74fa1a6a8a57bfaa7cb16
Carlos Castro <alu0100819847@ull.edu.es>
2019-04-11 11:27:11 +0100

    Esqueleto

---
+++ tabla.js
@@ -0,0 +1,4 @@
+var foto = ['6.jpg', '2.jpeg', '3.jpeg', '4.jpeg' , '5.jpg']
+for(var i = 0; i< $('.modal-body').length; i++){
+  $('.modal-body')[i].innerHTML = '<img class="foto" src="'+foto[i]+'"></img>'
+}
