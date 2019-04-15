f0fc897c81489f9843e203d1843d7b79df142f6e
Carlos Castro <alu0100819847@ull.edu.es>
2019-04-15 17:17:31 +0100

    New index

--- tabla.js
+++ tabla.js
@@ -1,4 +1,5 @@
-var foto = ['6.jpg', '2.jpeg', '3.jpeg', '4.jpeg' , '5.jpg']
+var foto = ['6.jpg', '2.jpeg', '3.jpeg', '4.jpeg', '5.jpg', '2.jpeg', '4.jpeg', '6.jpg', '2.jpeg', '5.jpg', '6.jpg', '2.jpeg', '3.jpeg', '4.jpeg', '5.jpg', '2.jpeg', '4.jpeg', '6.jpg', '2.jpeg', '5.jpg',]
+
 for(var i = 0; i< $('.modal-body').length; i++){
   $('.modal-body')[i].innerHTML = '<img class="foto" src="'+foto[i]+'"></img>'
 }
